d4e6057b8345688796869d8552071e6a1f9f5a80
Kenny <kfonatoko@gmail.com>
2021-05-28 19:54:47 -0400

    Add Recipes.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,80 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from "react";
-import { Accordion, Button, Card } from "react-bootstrap"
-import { CartX, Plus, Dash } from "react-bootstrap-icons"
+import { Accordion } from "react-bootstrap"
+import { CartX } from "react-bootstrap-icons"
 import axios from "axios";
-import { roundDecimal, simplifyUnits, addMeasurements } from "./Units.js";
 import { ExportModal } from "./Export.js";
-
-class Ingredients extends Component {
-    render() {
-        const results = this.props.ingredients.map((ingredient) => (
-            <tr key={ingredient.id} className={ ingredient.include ? "" : "table-secondary" }>
-                <td>
-                    <input
-                        type="checkbox"
-                        className="form-input"
-                        checked={ingredient.include}
-                        data-recipeid={ingredient.recipe}
-                        data-ingredientid={ingredient.id}
-                        onChange={this.props.toggleIngredient} />
-                </td>
-                <td className="text-right">
-                    {ingredient.quantity}
-                </td>
-                <td>
-                    {ingredient.measurement}
-                </td>
-                <td>
-                    {ingredient.ingredient}
-                </td>
-                <td>
-                    {ingredient.prepared}
-                </td>
-            </tr>
-        ));
-        return results;
-    }
-}
-
-class Recipes extends Component {
-    render() {
-        return this.props.recipes.map((recipe) => (
-            <Card>
-                <Card.Header>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-9">
-                                <Accordion.Toggle as={Button} variant="link" eventKey={recipe.id}>
-                                    <a data-toggle="collapse" data-parent="#recipe-list" href={"#collapse" + recipe.id}>
-                                        {recipe.name}
-                                    </a>
-                                </Accordion.Toggle>
-                            </div>
-                            <div className="col-3 btn-group text-right">
-                                <button type="button" className="btn btn-secondary" onClick={() => this.props.removeFromBasket(recipe.id)}><Dash /></button>
-                                <div className="btn btn-secondary">{recipe.quantity}</div>
-                                <button type="button" className="btn btn-secondary" onClick={() => this.props.addToBasket(recipe.id)}><Plus /></button>
-                            </div>
-                        </div>
-                    </div>
-                </Card.Header>
-
-                <Accordion.Collapse eventKey={recipe.id}>
-                    <table className="card-table table">
-                        <tbody>
-                            <Ingredients
-                                ingredients={recipe.ingredients}
-                                toggleIngredient={this.props.toggleIngredient}
-                            />
-                        </tbody>
-                    </table>
-                </Accordion.Collapse>
-            </Card>
-        ));
-    }
-}
+import { Recipes } from "./Recipes.js";
+import { roundDecimal, simplifyUnits, addMeasurements } from "./Units.js";
 
 class Basket extends Component {
     renderMeasurements(measurementSum) {
